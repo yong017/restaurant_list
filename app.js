529b78f41c9c6689436fc892409cef3ebcab2b95
yong017 <zero0lemon@gmail.com>
2022-11-28 21:18:21 +0800

    add handlebars

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+const exphbs = require('express-handlebars')
+//載入express-handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+//把引琴設定成hbs
+
+app.get('/', (req, res) => {
+  // res.send(`hi hello`)
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`working on ${port}`)
+})
