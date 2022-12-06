065434e54890bb1c8a7a106a69574246ad51bba6
yong017 <zero0lemon@gmail.com>
2022-12-07 00:09:33 +0800

    add main css

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurantData = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
   })
   res.render('index', { restaurants: restaurantData, keyword: keyword })
 })
