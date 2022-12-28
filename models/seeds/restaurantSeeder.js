05ca4b8559a1753ed9d2ae54615937522569c0dd
yong017 <zero0lemon@gmail.com>
2022-12-28 23:36:06 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant') // 載入  model
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (let i = 0; i < 10; i++) {
+    Restaurant.create({ name: `name-${i}` })
+  }
+  console.log('done')
+})
